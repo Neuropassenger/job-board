127fbfad7a0c28a110f013f26ad258e82b6b2faa
neuropassenger <turgenoid@gmail.com>
2024-04-22 11:41:33 +0400

    Create a very basic router

---
+++ controllers/error/404.php
@@ -0,0 +1,1 @@
+<?= 'Page not exists! 404!';
--- public/index.php
+++ public/index.php
@@ -1,3 +1,16 @@
 <?php
 require '../helpers.php';
-load_view('home');
+
+$routes = [
+    '/' => 'controllers/home.php',
+    '/listings' => 'controllers/listings/index.php',
+    '/listings/create' => 'controllers/listings/create.php',
+    '404' => 'controllers/error/404.php'
+];
+
+$uri = $_SERVER['REQUEST_URI'];
+if (key_exists($uri, $routes)) {
+    require base_path($routes[$uri]);
+} else {
+    require base_path($routes['404']);
+}
