7026dc3a3cd9380434326f6cb3bc8a6eed1689d1
neuropassenger <turgenoid@gmail.com>
2024-04-22 17:47:37 +0400

    Refactor the code, prepare db

--- public/index.php
+++ public/index.php
@@ -1,15 +1,21 @@
 <?php
 
+// Prepare the database for operation, if required
+//require base_path('prepare_database.php');
+
 require '../helpers.php';
-
-
 require base_path('Router.php');
+require base_path('Database.php');
 
+// Initialize the router
 $router = new Router();
 
+// Define routes
 require base_path('routes.php');
 
+// Get current URI and HTTP method
 $uri = $_SERVER['REQUEST_URI'];
 $method = $_SERVER['REQUEST_METHOD'];
 
+// Route the request
 $router->route($uri, $method);
