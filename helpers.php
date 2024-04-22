47a7341c878828d55d34703b09040a2bfc366a04
neuropassenger <turgenoid@gmail.com>
2024-04-22 11:03:56 +0400

    Add a view loader

--- helpers.php
+++ helpers.php
@@ -8,3 +8,13 @@
 function base_path($path = '') {
     return __DIR__ . '/' . $path;   
 }
+
+/** 
+ * Load a view
+ * 
+ * @param string $name
+ * @return void
+ */
+function load_view($name) {
+    require base_path('views/' . $name . '.php');
+}
