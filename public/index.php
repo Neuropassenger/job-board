a9eb3e60ab837b1706a7173a65b2c86df52a3e00
neuropassenger <turgenoid@gmail.com>
2024-04-22 12:11:30 +0400

    Split the router into separate files

--- public/index.php
+++ public/index.php
@@ -1,16 +1,7 @@
 <?php
+
 require '../helpers.php';
 
-$routes = [
-    '/' => 'controllers/home.php',
-    '/listings' => 'controllers/listings/index.php',
-    '/listings/create' => 'controllers/listings/create.php',
-    '404' => 'controllers/error/404.php'
-];
-
 $uri = $_SERVER['REQUEST_URI'];
-if (key_exists($uri, $routes)) {
-    require base_path($routes[$uri]);
-} else {
-    require base_path($routes['404']);
-}
+
+require base_path('router.php');
