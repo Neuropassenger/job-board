2a1b8fa4439db3884317d74abdd122a8c7a20f72
neuropassenger <turgenoid@gmail.com>
2024-04-22 11:22:24 +0400

    Add debug helpers

--- helpers.php
+++ helpers.php
@@ -17,6 +17,7 @@ function base_path($path = '') {
  */
 function load_view($name) {
     $view_path = base_path('views/' . $name . '.php');
+
     if (file_exists($view_path)) {
         require $view_path;
     } else {
@@ -32,9 +33,34 @@ function load_view($name) {
  */
 function load_partial($name) {
     $patial_path = base_path('views/partials/' . $name . '.php');
+    
     if (file_exists($patial_path)) {
         require $patial_path;
     } else {
         echo "Partial '{$name}' not found!";
     }
+}
+
+/**
+ * Inspect a value(s)
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect($value) {
+    echo '<pre>';
+    var_dump($value);
+    echo '</pre>';
+}
+
+/**
+ * Inspect value(s) and die
+ * 
+ * @param mixed $value
+ * @return void
+ */
+function inspect_and_die($value) {
+    echo '<pre>';
+    die(var_dump($value));
+    echo '</pre>';
 }
