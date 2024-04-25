d438ca7e1393554fec0da67f4d314e68fb8821bd
neuropassenger <turgenoid@gmail.com>
2024-04-25 13:09:22 +0400

    Display the tag field only when it is populated

--- App/views/listings/show.php
+++ App/views/listings/show.php
@@ -32,9 +32,11 @@
                 <li class="mb-2">
                     <strong>Location:</strong> <?= $listing->city . ($listing->state ? ', ' . $listing->state : ''); ?>
                 </li>
-                <li class="mb-2">
-                    <strong>Tags:</strong> <?= $listing->tags; ?>
-                </li>
+                <?php if (!empty($listing->tags)) : ?>
+                    <li class="mb-2">
+                        <strong>Tags:</strong> <?= $listing->tags; ?>
+                    </li>
+                <?php endif; ?>
             </ul>
         </div>
     </div>
